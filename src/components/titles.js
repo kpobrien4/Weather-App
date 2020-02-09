01fd269d9dbd034768cf5ddcf2d0d2cc3914a707
Kevin O'Brien <kpobrien@buffalo.edu>
2020-02-09 16:45:09 -0500

    built title component

---
+++ src/components/titles.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Titles extends React.Component {
+
+    render() {
+
+        return (
+
+            <div>
+
+                <h1 className="containerTitle">Weather Scanner</h1>
+                <p className="containerSubtitle"> Find weather conditions in your city</p>
+            </div>
+        )
+    }
+}
+
+export default Titles;
