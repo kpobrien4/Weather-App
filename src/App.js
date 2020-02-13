bad9111094d7b2f3d26dcdc5a114c9bd41166151
Kevin O'Brien <kpobrien@buffalo.edu>
2020-02-13 10:38:39 -0500

    imperial units

--- src/App.js
+++ src/App.js
@@ -23,9 +23,8 @@ class App extends React.Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     e.preventDefault();   
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&appid=${Api_Key}`);
     const response = await api_call.json();
-    console.log(response);
     if(city && country){
       this.setState({
         temperature: response.main.temp,
